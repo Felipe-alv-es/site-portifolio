dfb26c44605deadc5b5f40420c26e12a43c988ad
Felipe-alv-es <brago.sama@gmail.com>
2023-06-21 17:57:40 -0300

    chore(Site): Created home archive and installed typescript

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import getBackgroundStyle from "./Home.style.ts";
+
+const Home = () => {
+  return <div>Home aa</div>;
+};
+
+export default Home;
---
+++ src/pages/Home/Home.style.ts
@@ -0,0 +1,5 @@
+const getBackgroundStyle = () => {
+  return (
+    ''
+  )
+}
