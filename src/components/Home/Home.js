1d86c773b8ea1ec0ca89d515817756732b9ce822
Felipe-alv-es <brago.sama@gmail.com>
2023-06-22 01:29:54 -0300

    chore(Site): Created responsive initial base

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,35 @@
 import React from "react";
-import { StyledContainer } from "./Home.style.ts";
+import {
+  getContentStyle,
+  getContainerStyle,
+  getTitleStyle,
+  getSubtitleStyle,
+  getImageStyle,
+} from "./Home.style.ts";
+import { Typography, Box } from "@mui/material";
 
 const Home = () => {
   return (
-    <StyledContainer>
-      <h1>Desenvolvedor Front-End React</h1>
-      <p>
-        Olá, sou o Felipe. Apaixonado pelo desenvolvimento, trabalho a mais de
-        quatro anos na área de tecnologia, porém sempre aprendendo e me
-        dedicando principalmente a área de programação.
-      </p>
-    </StyledContainer>
+    <Box sx={getContainerStyle}>
+      <Box sx={getContentStyle}>
+        <Box sx={getTitleStyle}>
+          <Typography variant="h3">Desenvolvedor Front-End React</Typography>
+        </Box>
+        <Box sx={getSubtitleStyle}>
+          <Typography variant="subtitle2">
+            Olá, sou o Felipe. Apaixonado pelo desenvolvimento, trabalho a mais
+            de quatro anos na área de tecnologia, porém sempre aprendendo e me
+            dedicando principalmente a área de programação.
+          </Typography>
+        </Box>
+        <Box
+          sx={getImageStyle}
+          component="img"
+          src="https://img.freepik.com/vetores-premium/perfil-de-avatar-de-homem-no-icone-redondo_24640-14044.jpg?w=740"
+          alt="Man avatar generic image"
+        />
+      </Box>
+    </Box>
   );
 };
 
--- src/components/Home/Home.style.ts
+++ src/components/Home/Home.style.ts
@@ -1,6 +1,41 @@
-import styled from 'styled-components'
+import { SxProps } from "@mui/material/";
 
-export const StyledContainer = styled.div`
-  display: grid;
-  place-items: center;
-`
+export const getContainerStyle = (): SxProps => ({
+  display: "grid",
+  placeItems: "center",
+});
+
+export const getContentStyle = (): SxProps => ({
+  display: "grid",
+  gridTemplateAreas: `'title image''content image'`,
+  gridGap: "20px",
+  marginTop: "10%",
+  maxWidth: "900px",
+  "@media(max-width: 544px)": {
+    gridTemplateAreas: `'image''title''content'`,
+  },
+});
+
+export const getTitleStyle = (): SxProps => ({
+  maxWidth: "500px",
+  minWidth: "300px",
+  background: "red",
+  gridArea: "title",
+});
+
+export const getSubtitleStyle = (): SxProps => ({
+  maxWidth: "500px",
+  minWidth: "300px",
+  background: "green",
+  gridArea: "content",
+});
+
+export const getImageStyle = (): SxProps => ({
+  maxWidth: "200px",
+  minWidth: "150px",
+  maxHeight: "200px",
+  minHeight: "150px",
+  margin: "10px",
+  borderRadius: "300px",
+  gridArea: "image",
+});
