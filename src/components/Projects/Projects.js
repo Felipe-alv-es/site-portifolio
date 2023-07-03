a57162f19535965ba0cc97870745d5b47666cc30
Felipe-alv-es <brago.sama@gmail.com>
2023-07-03 11:51:28 -0300

    chore(Home): Added Figma option to homePage

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, IconButton, Divider, Tooltip } from "@mui/material";
+import { Box, Typography, IconButton, Divider } from "@mui/material";
 import {
   getContainerStyle,
   getImageStyle,
