fc8f61a4da9a6f90b8a58cd6a198539ee84145b0
Felipe-alv-es <brago.sama@gmail.com>
2023-09-14 13:00:49 -0300

    chore(ProjectPage): base of page

--- src/components/Projects/Pages/FictionalMobile.js
+++ src/components/Projects/Pages/FictionalMobile.js
@@ -1,7 +1,113 @@
 import React from "react";
+import { Box, Icon, IconButton, Typography } from "@mui/material";
+import { AiOutlineClose } from "react-icons/ai";
+import { GrReactjs } from "react-icons/gr";
 
 const FictionalMobile = () => {
-  return <div style={{ background: "coral" }}>FictionalMobile</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "8px 16px 8px 16px",
+        background: "#089cd4",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Typography
+          sx={{
+            color: "#e7e7e7",
+            typography: "h5",
+            fontWeight: "medium",
+            marginRight: "32px",
+          }}
+        >
+          FICTIONAL MOBILE
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            background: "#e7e7e7",
+            borderRadius: "16px",
+            padding: " 0px 12px 0px 12px",
+            marginRight: "4px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#089cd4",
+              typography: "overline",
+              fontWeight: "medium",
+              fontSize: "16px",
+              marginRight: "8px",
+              textTransform: "none",
+            }}
+          >
+            React
+          </Typography>
+          <Icon>
+            <GrReactjs color="#089cd4" />
+          </Icon>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            background: "#e7e7e7",
+            borderRadius: "16px",
+            padding: " 0px 12px 0px 12px",
+            marginRight: "4px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#089cd4",
+              typography: "overline",
+              fontWeight: "medium",
+              fontSize: "16px",
+              marginRight: "8px",
+              textTransform: "none",
+            }}
+          >
+            JavaScript
+          </Typography>
+          <Icon>
+            <GrReactjs color="#089cd4" />
+          </Icon>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            background: "#e7e7e7",
+            borderRadius: "16px",
+            padding: " 0px 12px 0px 12px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#089cd4",
+              typography: "overline",
+              fontWeight: "medium",
+              fontSize: "16px",
+              marginRight: "8px",
+              textTransform: "none",
+            }}
+          >
+            TypeScript
+          </Typography>
+          <Icon>
+            <GrReactjs color="#089cd4" />
+          </Icon>
+        </Box>
+      </Box>
+      <IconButton sx={{ width: "fit-content", height: "fit-content" }}>
+        <AiOutlineClose color="#e7e7e7" />
+      </IconButton>
+    </Box>
+  );
 };
 
 export default FictionalMobile;
