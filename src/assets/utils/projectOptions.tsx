7b6dafce276bc90d3032fc7362016852b02c2bae
Felipe-alv-es <brago.sama@gmail.com>
2024-05-02 12:40:48 -0300

    Better placeholder images

--- src/assets/utils/projectOptions.tsx
+++ src/assets/utils/projectOptions.tsx
@@ -2,19 +2,21 @@ import AlmaDS from "../../assets/images/ProjectsImages/AlmaDsImages/AlmaDesignSy
 import TwelveHours from "../../assets/images/ProjectsImages/TwelveHours/Imagem1.png";
 import FictionalMobileMobile from "../../assets/images/ProjectsImages/FictionalMobileImages/FictionalMobileMobile.png";
 import FictionalMobile from "../../assets/images/ProjectsImages/FictionalMobileImages/FictionalMobile.png";
+import TwelveHoursMobile from "../../assets/images/ProjectsImages/TwelveHours/TwelveHoursMobile.png";
+import AlmaDSMobile from "../../assets/images/ProjectsImages/AlmaDsImages/AlmaDSMobile.png";
 
 export const projectsOptions = [
   {
     title: "Técnica das 12 Horas",
     imageSrc: TwelveHours,
-    imageSrcMobile: FictionalMobileMobile,
+    imageSrcMobile: TwelveHoursMobile,
     imageAlt: "Homepage of TwelveHours Site",
     src: "/twelve-hours",
   },
   {
     title: "Alma Design System",
     imageSrc: AlmaDS,
-    imageSrcMobile: FictionalMobileMobile,
+    imageSrcMobile: AlmaDSMobile,
     imageAlt: "Logo do Alma DS",
     src: "/alma-ds",
   },
