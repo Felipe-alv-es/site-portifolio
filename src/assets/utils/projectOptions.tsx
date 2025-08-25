4f0343000382575e66395bf5ed29b2a0f00d0e5a
Felipe-alv-es <brago.sama@gmail.com>
2025-08-25 01:54:08 -0300

    chore: updated felit cursos

--- src/assets/utils/projectOptions.tsx
+++ src/assets/utils/projectOptions.tsx
@@ -1,6 +1,7 @@
 import AlmaDS from "../../assets/images/ProjectsImages/AlmaDsImages/AlmaDesignSystem.png";
 import AlmaDSMobile from "../../assets/images/ProjectsImages/AlmaDsImages/AlmaDSMobile.png";
 import FelitCourses from "../../assets/images/ProjectsImages/LandingPageImages/FelitCourses.png";
+import FelitCoursesMobile from "../../assets/images/ProjectsImages/LandingPageImages/FelitCoursesMobile.jpg";
 import Encanto from "../../assets/images/ProjectsImages/EncantoArtesanal/Encanto1.png";
 import EncantoMobile from "../../assets/images/ProjectsImages/EncantoArtesanal/EncantoMobile.jpg";
 
@@ -15,7 +16,7 @@ export const projectsOptions = [
   {
     title: "Cursos de TI",
     imageSrc: FelitCourses,
-    imageSrcMobile: AlmaDSMobile,
+    imageSrcMobile: FelitCoursesMobile,
     imageAlt: "Homepage of FelitCourses Site",
     src: "/felit-courses",
   },
